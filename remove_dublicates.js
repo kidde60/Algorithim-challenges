9ac9dcb0c76bb516c9cb4dc544969d5630bd45d7
GEORGE WILLIAM KIDDE <kgeorgewilliam60@gmail.com>
2023-06-10 23:39:21 +0300

    Update remove_dublicates.js

--- remove_dublicates.js
+++ remove_dublicates.js
@@ -2,7 +2,6 @@ var removeDuplicates = function(nums) {
     let noDup= new Set(nums);
     let newArr=[];
     newArr.push(...noDup);
-    
     return newArr;
 };
 console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
